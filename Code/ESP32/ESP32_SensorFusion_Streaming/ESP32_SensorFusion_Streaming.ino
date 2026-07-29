0201f42628c7909b672159be6a1b62dbedd9d7ba
Abdelrahman Hewala <abdelrahmanhewala@Abdelrahmans-MacBook-Pro.local>
2026-07-29 21:24:25 +0200

    adding UART Configuration to match ESP's

--- Code/ESP32/ESP32_SensorFusion_Streaming/ESP32_SensorFusion_Streaming.ino
+++ Code/ESP32/ESP32_SensorFusion_Streaming/ESP32_SensorFusion_Streaming.ino
@@ -1,12 +1,15 @@
 #include <Wire.h>
 #include "ICM20948_SPI.h"
 #include "IMUTypes.h"
-
+#include "UART_Streaming.h"
 
 #include "ICM20948_SPI.h"
 
+#define SERIALCONFIG SERIAL_8O1
+#define SERIALRATE 115200
+
 void setup() {
-    Serial.begin(115200);
+    Serial.begin(SERIALRATE, SERIALCONFIG);
     delay(1000);
 
     int result = ICM20948::runningSetup();
@@ -18,6 +21,8 @@ void setup() {
 }
 
 void loop() {
+    IMUPhysical imu = ICM20948::readStreamData();
+
     // IMUPhysical p = ICM20948::readStreamData();
     // Serial.printf(
     //     "accel[%.3f, %.3f, %.3f] gyro[%.4f, %.4f, %.4f] mag[%.2f, %.2f, %.2f] valid=%d temp=%.1f\n",
@@ -28,9 +33,19 @@ void loop() {
     // );
 
 
-    BenchResult r = ICM20948::benchmarkReadStreamData(1000);    // 1000 sample
-    ICM20948::printBenchResult("readStreamData", r);
-    delay(1000);   // slow print rate for readability — real loop will run much faster
+    // BenchResult r = ICM20948::benchmarkReadStreamData(1000);    // 1000 sample
+    // ICM20948::printBenchResult("readStreamData", r);
+    // delay(1000);   // slow print rate for readability — real loop will run much faster
+
+    sendIMUPacket(
+        imu.accel[0], imu.accel[1], imu.accel[2],
+        imu.gyro[0], imu.gyro[1], imu.gyro[2],
+        imu.mag[0], imu.mag[1], imu.mag[2],
+        0, 0, 0,
+        0, 0, 0,
+        imu.magValid
+    );
+    delay(1000);
 }
 
 
